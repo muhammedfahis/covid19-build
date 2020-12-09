ccc2738d274f2b66caaed741a264ece3ff73b09c
muhammedfahis <fahisccc3@gmail.com>
2020-12-09 22:54:30 +0530

    build file of react covid app

---
+++ build/precache-manifest.e82260212a1b7c84fb23ac35e40649f5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad9da26e48b069806d6581820387500d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d60939d827fd9498cb9",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "5fe78ab9e0038436c78a",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6d60939d827fd9498cb9",
+    "url": "/static/js/2.04662f97.chunk.js"
+  },
+  {
+    "revision": "0fc61ce0ee2d491f41ee7634c63d9b90",
+    "url": "/static/js/2.04662f97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fe78ab9e0038436c78a",
+    "url": "/static/js/main.0bd0b9f7.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
